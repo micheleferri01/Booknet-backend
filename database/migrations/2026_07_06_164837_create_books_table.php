9c6d9aca0fe6ae7155b74db7d1f61e56b938906b
micheleferri01 <micheleferri095@gmail.com>
2026-07-07 21:54:06 +0200

    add: db migrations to create tables books, authors and editors

---
+++ database/migrations/2026_07_06_164837_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('cover')->nullable();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->year('publication_year');
+            $table->text('plot')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->foreignId('author_id')->constrained();
+            $table->foreignId('editor_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
