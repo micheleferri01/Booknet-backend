23ee288d4aefe7554ea20e38711a53b32ab01c4c
micheleferri01 <micheleferri095@gmail.com>
2026-07-07 13:17:34 +0200

    add: slug creation to model book and override of the route key name

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,14 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Override;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Book extends Model
 {
+    use HasSlug;
+
+    public function getSlug(){
+        return SlugOptions::create()->generateSlugsFrom('title')->saveSlugsTo('slug');
+    }
+
     public function author(){
         return $this->belongsTo(Author::class);
     }
 
     public function editor(){
         return $this->belongsTo(Editor::class);
+    }
+
+    #[Override]
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
