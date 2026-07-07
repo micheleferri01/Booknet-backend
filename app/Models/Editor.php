f5e7c9052e6f4d6b9ed44614992c7b36ecb755fd
micheleferri01 <micheleferri095@gmail.com>
2026-07-07 21:52:39 +0200

    add: slug logic to models Author and Editor

--- app/Models/Editor.php
+++ app/Models/Editor.php
@@ -3,10 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Override;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Editor extends Model
 {
+    use HasSlug;
+    
+    public function getSlug()
+    {
+        return SlugOptions::create()->generateSlugsFrom('title')->saveSlugsTo('slug');
+    }
+
     public function books(){
         return $this->hasMany(Book::class);
+    }
+
+    #[Override]
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
