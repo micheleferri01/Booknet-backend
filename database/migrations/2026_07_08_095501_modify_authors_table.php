e700b462c321feaf525c534cbbf4ffe72dc87800
micheleferri01 <micheleferri095@gmail.com>
2026-07-08 16:22:32 +0200

    update: authors table structure

---
+++ database/migrations/2026_07_08_095501_modify_authors_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('authors', function (Blueprint $table){
+            $table->string('name')->unique()->change();
+            $table->string('slug')->unique()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('authors', function (Blueprint $table) {
+            $table->string('name')->change();
+            $table->string('slug')->change();
+        });
+    }
+};
