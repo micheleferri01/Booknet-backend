65ac6fc4c4159c8deed5ab9ff3ec59b6c5beff25
micheleferri01 <micheleferri095@gmail.com>
2026-07-08 11:52:08 +0200

    add: editors seeder

---
+++ database/seeders/EditorsSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Editor;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EditorsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $editors = [
+            'Bompiani',
+            'Salani',
+            'Fanucci',
+            'Mondadori',
+            'Einaudi',
+            'Rizzoli',
+            'Newton Compton',
+            'Feltrinelli',
+            'Sperling & Kupfer',
+            'Piemme',
+            'Garzanti',
+            'De Agostini'
+        ];
+
+        foreach($editors as $editor){
+            $newEditor = new Editor();
+            $newEditor->name = $editor;
+            $newEditor->save();
+        }
+    }
+}
