f8de4060f7246be3259d3923f0240113683afe59
micheleferri01 <micheleferri095@gmail.com>
2026-07-12 23:05:12 +0200

    add: import of css file for tom-select

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap';
 import '~resources/scss/app.scss';
 import '~icons/bootstrap-icons.scss';
 import * as bootstrap from 'bootstrap';
+import 'tom-select/dist/css/tom-select.bootstrap5.css';
 import.meta.glob([
     '../img/**'
 ])
