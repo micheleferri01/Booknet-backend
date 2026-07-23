82c1667dfb4ad4dae8153710bcd73577dbbbc7e1
micheleferri01 <micheleferri095@gmail.com>
2026-07-23 23:23:45 +0200

    add: books list in genres method show

---
+++ resources/views/genres/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+<h1 class="py-3">{{$genre->name}}</h1>
+<div id="books-list" class="row g-3 pb-4">
+    @include('books.partials.books_list')
+</div>
+@endsection
