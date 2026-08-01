56a5d8bf1cf32742535b3ba75c1dcea7d03eb7c2
micheleferri01 <micheleferri095@gmail.com>
2026-08-01 22:07:30 +0200

    update: logic of api books route index

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -33,17 +33,10 @@ class BookController extends Controller
             });
         })->orderBy('title')->get();
 
-        if(count($books) == 0){
-            return response()->json([
-                'success' => false,
-                'message' => 'Nessun libro trovato con i parametri inseriti.'
-            ], 404);
-        }else{ 
-            return response()->json([
-                'success'=> true,
-                'data'=> $books
-            ], 200);
-        }
+        return response()->json([
+            'success'=> true,
+            'data'=> $books
+        ], 200);
 
     }
 
