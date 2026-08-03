c746f7c5f91286873587ef7874b297d19e3a0f94
micheleferri01 <micheleferri095@gmail.com>
2026-08-03 18:15:22 +0200

    add: CartController

---
+++ app/Http/Controllers/Api/CartController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function index(Request $request)
+    {
+        $data = $request->validate([
+            'books' => ['required', 'array'],
+            'books.*.id' => ['required', 'integer'],
+            'books.*.quantity' => ['required', 'integer', 'min:1'],
+        ]);
+
+        $items = collect($data['books']);
+
+        $books = Book::with(['author', 'editor', 'genres'])
+            ->whereIn('id', $items->pluck('id'))
+            ->get();
+
+        $books->each(function ($book) use ($items) {
+
+            $item = $items->firstWhere('id', $book->id);
+
+            $book->quantity = $item['quantity'];
+
+        });
+
+        return response()->json([
+            'success' => true,
+            'data' => $books
+        ]);
+    }
+}
